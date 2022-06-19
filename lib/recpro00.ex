0b205dd06da976df6dd6be4ad5cb8a01bda563be
Howard Bussey <howard.bussey@gmail.com>
2022-06-19 15:48:22 -0400

    initial

---
+++ lib/recpro00.ex
@@ -0,0 +1,18 @@
+defmodule Recpro00 do
+  @moduledoc """
+  Documentation for `Recpro00`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Recpro00.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/recpro00_test.exs
@@ -0,0 +1,8 @@
+defmodule Recpro00Test do
+  use ExUnit.Case
+  doctest Recpro00
+
+  test "greets the world" do
+    assert Recpro00.hello() == :world
+  end
+end
