792bcdc600957937159459ab007bec7eeb601b83
Howard Bussey <howard.bussey@gmail.com>
2022-08-27 21:55:36 -0400

    get working, add boxcar1

--- lib/recpro00.ex
+++ lib/recpro00.ex
@@ -16,7 +16,10 @@ defmodule Recpro00 do
       iex> Recpro00.example1()
       {:ok, "example1"}
 
-  """
+      iex> Recpro00.example2()
+      {:ok, "example2"}
+
+      """
 
   def example0 do
     background = get_background()
@@ -41,24 +44,56 @@ defmodule Recpro00 do
     {:ok, "example1"}
   end
 
-  def execute_task(task, %{dict: dict} = _info) do
+  def example2 do
+    background = get_background()
+
+    new_task("boxcar1.csv", "test2", "test of boxcar1", "test_boxcar1")
+    |> execute_task(background)
+    |> IO.inspect(label: "boxcar1")
+    {:ok, "example2"}
+  end
+
+  def execute_task(task, %{dict: dict, phonemes: phonemes} = _info) do
     # IO.inspect(Map.get(dict, "you", :not_present), label: "you...")
     corpus = get_corpus(task.corpus_file)
-    missing_words = Enum.filter(corpus, fn word -> not Map.has_key?(dict, word) end)
-    frequencies = Enum.map(dict, fn a -> {a, 0} end)
+    missing_words = Enum.filter(corpus, fn word -> not Map.has_key?(dict, elem(word, 0)) end)
+
+    frequencies =
+      Enum.map(phonemes, fn a -> {a, 0} end)
+      |> Enum.into(%{})
+
+    Enum.take(corpus, 5)
+    # |> IO.inspect(label: "corpus")
+    # IO.inspect(frequencies, label: "frequencies initial")
 
     sounds =
       Enum.reduce(corpus, frequencies, fn word, freqs ->
-        update_frequencies(word, frequencies, dict)
+        update_frequencies(word, freqs, dict)
       end)
 
-    frequencies = Enum.frequencies(sounds) |> Map.to_list()
+    # Enum.take(sounds, 5) |> IO.inspect(label: "sounds")
+    # frequencies = Enum.frequencies(sounds) |> Map.to_list()
+    frequencies = sounds
     IO.inspect(missing_words, label: "missing words")
-    Enum.sort(frequencies, fn {_k1, v1}, {_k2, v2} -> v1 >= v2 end)
+    rv = Enum.sort(frequencies, fn {_k1, v1}, {_k2, v2} -> v1 >= v2 end)
+    # Enum.take(rv, 5) |> IO.inspect(label: "sorted frequencies")
+    rv
   end
 
   def update_frequencies({spelling, count} = _word, frequency_map, dict) do
+    # IO.inspect({spelling, count}, label: "word in update_frequencies")
+    # IO.inspect(frequency_map, label: "map in update_frequencies")
+
     phonemes = Map.get(dict, spelling, ~w/zz yy xx/)
+    # |> IO.inspect(label: "phonemes in update_frequencies")
+
+    new_frequency_map =
+      Enum.reduce(phonemes, frequency_map, fn phoneme, fm ->
+        Map.update(fm, phoneme, count, fn value -> value + count end)
+      end)
+
+    # IO.inspect("have new frequency map")
+    new_frequency_map
   end
 
   def get_corpus(file_name) do
@@ -83,11 +118,9 @@ defmodule Recpro00 do
     |> File.read!()
     |> String.split()
     |> CSV.decode!(headers: true)
-    |> Enum.map(fn a -> {String.downcase(Map.get(a, "Word")), Map.get(a, "Frequency")} end)
-    |> Enum.take(5)
-    |> IO.inspect(label: "csv content")
-
-    [{"the", 1}]
+    |> Enum.map(fn a ->
+      {String.downcase(Map.get(a, "Word")), Map.get(a, "Frequency") |> String.to_integer()}
+    end)
   end
 
   # corpus file (e.g. Fry List)
@@ -118,6 +151,7 @@ defmodule Recpro00 do
       |> Enum.reduce([], fn dict_entry, acc ->
         (dict_entry |> tl |> hd |> String.split()) ++ acc
       end)
+      |> unstressed_arpabet()
       |> Enum.uniq()
 
     # |> IO.inspect(label: "phonemes")
